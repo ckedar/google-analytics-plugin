433c64159b0f919e4f0b0b3d5261231f769878a6
sandeep dillerao <sandydillerao@gmail.com>
2016-12-09 13:25:43 +0530

    GoogleAdWords Conversion Tracking BroadcastReceiver added

--- android/CustomReceiver.java
+++ android/CustomReceiver.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 
 import android.os.Bundle;
 import android.util.Log;
+import com.google.ads.conversiontracking.AdWordsConversionReporter;
 import com.google.android.gms.analytics.CampaignTrackingReceiver;
 
 /*
